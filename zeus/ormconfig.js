ab3c31266afc89769409e8527c958dca3c3d5875
Bruno Felipe <bfaa1999@gmail.com>
2020-05-12 23:02:28 -0300

    v4

---
+++ zeus/ormconfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  dropSchema: false,
+  // logging: true,
+  ssl: true,
+  entities: ['src/app/models/**/*.ts'],
+  migrations: ['src/database/migrations/**/*.ts'],
+  subscribers: ['src/database/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/app/models',
+    migrationsDir: 'src/database/migrations',
+    subscribersDir: 'src/database/subscriber',
+  },
+};
