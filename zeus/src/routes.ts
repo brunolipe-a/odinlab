3095c9168466d4b1830d6fe715c1315813d3bf9e
Bruno Felipe <bfaa1999@gmail.com>
2020-05-12 22:16:21 -0300

    add to test

---
+++ zeus/src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import auth from './app/middlewares/auth';
+import UserController from './app/controllers/UserController';
+import SessionController from "./app/controllers/SessionController";
+
+const routes = Router();
+
+routes.get('/', (req, res) => {
+  return res.status(200).json({ message: "Zeus is ok, thanks!!!!" })
+});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(auth);
+
+routes.get('/users', UserController.index);
+
+export default routes;
